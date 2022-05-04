62fc4705cfe424b57c3fd86b389efc8024c21cea
luciechevarria3 <lechevarria009@ikasle.ehu.eus>
2022-05-04 15:46:26 +0200

    Agregar archivos de proyecto.

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Luciano Echevarria\Documents\INFORMATICA\SEGUNDOCUATRI\HADS\LABS\LAB9\CineDjangoStudents\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
